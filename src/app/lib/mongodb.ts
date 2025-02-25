098d120118b51c9bcb922ec5c6d7276fb08ff526
Virendra <59563175+nVirendra@users.noreply.github.com>
2025-02-25 16:37:32 +0530

    connection upt

--- src/app/lib/mongodb.ts
+++ src/app/lib/mongodb.ts
@@ -13,7 +13,7 @@ export const connectDB = async () => {
   
   if (!cached.promise) {
     cached.promise = mongoose.connect(MONGODB_URI, {
-      dbName: "fixhr",
+      dbName: "face_detection",
       bufferCommands: false,
     }).then((mongoose) => mongoose);
   }
